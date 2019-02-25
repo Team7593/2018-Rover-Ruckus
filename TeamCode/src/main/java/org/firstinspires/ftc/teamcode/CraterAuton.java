d38d0ba01486d6c6b91ca759d4bfc3e2b75a569a
vineelavandanapu <onewitchcat@gmail.com>
2019-02-24 17:30:45 -0800

    made a small change to auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CraterAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CraterAuton.java
@@ -43,7 +43,7 @@ public class CraterAuton extends Team7593Opmode {
         steps.add(new DriveX(.2, .75)); //drive to be in the middle
 
         steps.add(new GoldStrafe()); //strafe to the side to line up with the gold
-        steps.add(new DriveY(1, .95)); //drive forward and knock the right mineral off
+        steps.add(new DriveY(.6, .95)); //drive forward and knock the right mineral off
         steps.add(new DriveY(.40, -.95));//drive backward; makes sure we are clear when we turn
         return steps;
     }
